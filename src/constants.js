7d294e627fdae2e56570b6573ff54b07a459d98c
Declan de Wet <declandewet@me.com>
2016-11-02 00:50:55 +0200

    don't perform client-side render if data-vue-meta-server-rendered attribute exists on html tag

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const VUE_META_ATTRIBUTE = 'data-vue-meta'
+export const SERVER_RENDERED_ATTRIBUTE = 'data-vue-meta-server-rendered'
--- src/updateClientMetaInfo.js
+++ src/updateClientMetaInfo.js
@@ -1,5 +1,6 @@
 import updateTitleTag from './updateTitleTag'
 import updateHtmlTagAttributes from './updateHtmlTagAttributes'
+import { SERVER_RENDERED_ATTRIBUTE } from './constants'
 
 /**
  * Performs client-side updates when new meta info is received
@@ -7,10 +8,18 @@ import updateHtmlTagAttributes from './updateHtmlTagAttributes'
  * @param  {Object} newInfo - the meta info to update to
  */
 export default function updateClientMetaInfo (newInfo) {
-  if (newInfo.title) {
-    updateTitleTag(newInfo.title)
-  }
-  if (newInfo.htmlAttrs) {
-    updateHtmlTagAttributes(newInfo.htmlAttrs)
+  const htmlTag = document.getElementsByTagName('html')[0]
+
+  // if this is not a server render, then update
+  if (htmlTag.getAttribute(SERVER_RENDERED_ATTRIBUTE) === null) {
+    if (newInfo.title) {
+      updateTitleTag(newInfo.title)
+    }
+
+    if (newInfo.htmlAttrs) {
+      updateHtmlTagAttributes(newInfo.htmlAttrs)
+    }
+  } else {
+    htmlTag.removeAttribute(SERVER_RENDERED_ATTRIBUTE)
   }
 }
