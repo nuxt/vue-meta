4dfa2e0e550ba548f5f17f77fb49b8dc175cf05d
pimlie <pimlie@hotmail.com>
2019-03-11 17:17:13 +0100

    test: add nav-guard tests for refresOnce false

--- src/shared/nav-guards.js
+++ src/shared/nav-guards.js
@@ -3,6 +3,7 @@ import { isFunction } from '../utils/is-type'
 export function addNavGuards(vm) {
   // return when nav guards already added or no router exists
   if (vm.$root._vueMeta.navGuards || !vm.$root.$router) {
+    /* istanbul ignore next */
     return
   }
 
