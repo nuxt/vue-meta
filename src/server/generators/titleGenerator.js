90e4493564fd277ea62d2dae27d44b6ba4d237fb
Clark Du <clark.duxin@gmail.com>
2019-03-19 16:23:59 +0000

    fix: not generate title tag if title is empty

--- src/server/generators/titleGenerator.js
+++ src/server/generators/titleGenerator.js
@@ -11,7 +11,7 @@ export default function _titleGenerator (options = {}) {
   return function titleGenerator (type, data) {
     return {
       text () {
-        return `<${type} ${attribute}="true">${data}</${type}>`
+        return data ? `<${type} ${attribute}="true">${data}</${type}>` : ''
       }
     }
   }
