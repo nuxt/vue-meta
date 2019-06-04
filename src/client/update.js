21ec039a5c0e276bedd2782751f54a7d734967fb
pimlie <pimlie@hotmail.com>
2019-06-04 20:35:38 +0200

    fix: detect and apply changes happened before or during initialization

--- src/client/update.js
+++ src/client/update.js
@@ -2,6 +2,13 @@
 let batchId = null
 
 export function triggerUpdate(vm, hookName) {
+  // if an update was triggered during initialization or when an update was triggered by the
+  // metaInfo watcher, set initialized to null
+  // then we keep falsy value but know we need to run a triggerUpdate after initialization
+  if (!vm.$root._vueMeta.initialized && (vm.$root._vueMeta.initializing || hookName === 'watcher')) {
+    vm.$root._vueMeta.initialized = null
+  }
+
   if (vm.$root._vueMeta.initialized && !vm.$root._vueMeta.paused) {
     // batch potential DOM updates to prevent extraneous re-rendering
     batchUpdate(() => vm.$meta().refresh())
