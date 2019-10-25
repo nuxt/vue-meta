b8519d04495ec31ec06f46bb739281118bbc2f1e
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-10-25 13:22:12 -0300

    fix: afterNavigation

--- src/shared/nav-guards.js
+++ src/shared/nav-guards.js
@@ -19,10 +19,12 @@ export function addNavGuards (rootVm) {
   })
 
   router.afterEach(() => {
-    const { metaInfo } = resume(rootVm)
+    rootVm.$nextTick(function () {
+      const { metaInfo } = resume(rootVm)
 
-    if (metaInfo && isFunction(metaInfo.afterNavigation)) {
-      metaInfo.afterNavigation(metaInfo)
-    }
+      if (metaInfo && isFunction(metaInfo.afterNavigation)) {
+        metaInfo.afterNavigation(metaInfo)
+      }
+    })
   })
 }
