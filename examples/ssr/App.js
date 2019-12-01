ead04e9c65625436ba8340e563cd6964108b86f8
pimlie <pimlie@hotmail.com>
2019-12-01 14:54:54 +0100

    chore: remove debug info

--- examples/ssr/App.js
+++ examples/ssr/App.js
@@ -8,27 +8,11 @@ Vue.use(VueMeta, {
 })
 
 export default function createApp () {
-  const SomeComponent = {
-    name: 'SomeComponent',
-    template: `<div>
-      <p>Some Component</p>
-    </div>`,
-    mounted() {
-      console.log('some mounted')
-    }
-  }
-
   const Home = {
-    name: 'home',
-    components: {
-      SomeComponent
-    },
     template: `<div>
       <router-link to="/about">About</router-link>
 
       <p>Hello World</p>
-
-      <some-component v-once></some-component>
     </div>`,
     metaInfo: {
       title: 'Hello World',
@@ -48,7 +32,6 @@ export default function createApp () {
   }
 
   const About = {
-    name: 'About',
     template: `<div>
       <router-link to="/">Home</router-link>
 
