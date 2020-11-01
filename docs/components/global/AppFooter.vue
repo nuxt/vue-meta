ecc8787111d3c4b16bfa4e6b0e6cbc4d48e845fc
Daniel Roe <daniel@parentscheme.com>
2020-11-01 15:50:24 +0000

    fix: add footer with attribution

---
+++ docs/components/global/AppFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer
+    class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20"
+  >
+    <div
+      class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between"
+    >
+      <div class="flex items-end">
+        <a href="https://nuxtjs.org" target="_blank" rel="noopener">
+          <BuiltWithNuxtLight class="h-4 light-img" />
+          <BuiltWithNuxtDark class="h-4 dark-img" />
+        </a>
+      </div>
+      <div class="flex">
+        <AppLangSwitcher />
+        <AppColorSwitcher class="ml-4" />
+      </div>
+    </div>
+    <div
+      class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between"
+    >
+      MIT License | Created by Declan de Wet, currently developed with ❤ by
+      Nuxt.js core-team & contributors
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale);
+    },
+  },
+};
+</script>
