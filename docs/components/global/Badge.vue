5d4862d1a25a9b389da0d08be5a8164f368f9c8b
Daniel Roe <daniel@parentscheme.com>
2020-11-01 15:23:16 +0000

    docs: add orange runtime badges

---
+++ docs/components/global/Badge.vue
@@ -0,0 +1,16 @@
+<template functional>
+  <span
+    :class="`bg-${props.color}-100 dark:bg-${props.color}-900 text-${props.color}-500`"
+    class="inline-flex items-center px-3 py-1 leading-5 rounded-full text-base tracking-tight"
+  >
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    color: { type: String, default: "primary" },
+  },
+};
+</script>
