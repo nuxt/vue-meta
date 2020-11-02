bb1f6a1eca23d9d6432fd9e83eebd662e7b28c19
Daniel Roe <daniel@roe.dev>
2020-11-02 12:18:41 +0000

    fix: improve styling for introduction menu item

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,6 +1,9 @@
+import path from 'path'
+
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+  css: [path.resolve(__dirname, './assets/custom.css')],
   generate: {
     routes: ['/'],
     dir: '.vuepress/dist'
